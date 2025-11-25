bf2916c6a88d1d9ecafc892f04860d1e71b83e15
RaeBaek <bmp117@naver.com>
2025-11-25 23:34:05 +0900

    chore: Streaming 하위 각 모듈 Tests 폴더 추가

---
+++ Features/Streaming/StreamingData/Tests/ShowcaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShowcaseTests.swift
+//  Showcase
+//
+//  Created by 백래훈 on 10/23/25.
+//
+
+import Testing
+@testable import Showcase
+
+struct ShowcaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
